7ef60be7c141309464cda336a77826c6ca122883
Stuart Fong <stuartfong@Admins-MacBook-Pro.local>
2020-05-23 12:33:58 -0400

    set up a redirect rule

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,9 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
-    '/*',
+    '/api',
     createProxyMiddleware({
-      target: 'https://appletree-express-server.herokuapp.com:3000',
+      target: 'http://localhost:3000',
       changeOrigin: true,
     })
   );
